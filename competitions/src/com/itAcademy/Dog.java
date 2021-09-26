8a779b60722f904f84542e73228392deef819225
elentkachenko <77787424+Elena23Tkachenko@users.noreply.github.com>
2021-09-26 19:42:11 +0300

    hw4

--- competitions/src/com/itAcademy/Dog.java
+++ competitions/src/com/itAcademy/Dog.java
@@ -9,27 +9,5 @@ class Dog extends Animal {
     final int MAX_SWIM_DISTANCE = 10;
     final double MAX_JUMP_HEIGHT = 0.5;
 
-
-    Dog maya = new Dog("Собака", "Майя", 450, 9, 0.3);
-    Dog rex = new Dog("Собака", "Рекс", 300, 30, 0.2);
-
-//    private final int MAX_RUN_DISTANCE = 120;
-//    private final int MAX_SWIM_DISTANCE = 50;
-//    private final double MAX_JUMP_HEIGHT = 2.3;
-//
-//    public int getMAX_RUN_DISTANCE() {
-//        return MAX_RUN_DISTANCE;
-//    }
-//
-//    public double getMAX_JUMP_HEIGHT() {
-//        return MAX_JUMP_HEIGHT;
-//    }
-//
-//    public int getMAX_SWIM_DISTANCE() {
-//        return MAX_SWIM_DISTANCE;
-//    }
-//
-
 }
-
 
--- competitions/src/com/itAcademy/Team.java
+++ competitions/src/com/itAcademy/Team.java
@@ -1,19 +1,15 @@
 package com.itAcademy;
 
 public class Team {
-    public String name;
-    public String participants;
-
-    public Team(String name, String participants) {
-        this.name = name;
-        this.participants = participants;
-
-
+    public Team(String teamName) {
+        Team team = new Team("Огонь");
+        Animal[] competitors = new Animal[4];
+        competitors[0] = new Cat("Кот", "Лучик", 199, 0, 1.9);
+        competitors[1] = new Cat("Кот", "Мурзик", 155, 0, 1.2);
+        competitors[2] = new Dog("Собака", "Майя", 450, 9, 0.3);
+        competitors[3] = new Dog("Собака", "Рекс", 300, 30, 0.2);
     }
-   
+
 }
-
-
-
 
 
